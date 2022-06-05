333bcddc1989831d158c9ee3d2f9ca92e0a6ac45
NThoron-UCD <nthoron@ucdavis.edu>
2022-06-04 20:35:23 -0700

    Committing some simple changes

--- Exploratory Analysis.R
+++ Exploratory Analysis.R
@@ -6,6 +6,11 @@ library(broom)
 library(mgcv)
 
 county_data <- fread("Data/Pavements & LOSTs - Sheet 1.csv")
+county_data <- subset(county_data, !is.na(CZP1))
+county_data$`Most Common Climate Zone` <- as.factor(county_data$`Most Common Climate Zone`)
+county_data[is.na(county_data$`Average Streets & Roads Percent`)]$`Average Streets & Roads Percent` <- 0
+county_data[is.na(county_data$`Average Local Return Percent`)]$`Average Local Return Percent` <- 0
+county_data$`Pop per centerline miles` <- county_data$Population/county_data$`Centerline Miles`
 summary(county_data)
 
 county_data <- county_data %>% mutate(
@@ -67,12 +72,31 @@ summary(lm(`Change in PCI` ~
            , data = county_data))
 
 # Stepwise regression
-full.model <- lm(`Change in PCI` ~ `Centerline Miles` + `Lane-miles` + `Area (sy)` + `Area (sm)` + `PCI 2008` + Population + `LOST Active between 08-20` + `Measure expired and replaced?` + `Period Active` + `LOST Rate` + `Multiple LOSTS Active at same time?` + `Average Streets & Roads Percent` + `Average Local Return Percent` + `Streets Metric` + `Local Return Metric` + `Did the PCI start below 60?` + `Did the PCI fall below 60?` + `Pop per lane miles` + `Most Common Climate Zone` + `Climate Zone Percentage` + CZP1 + CZP2 + CZP3 + CZP4 + CZP5 + CZP6 + CZP7 + CZP8 + CZP9 + CZP10 + CZP11 + CZP12 + CZP13 + CZP14 + CZP15 + CZP16, data = county_data)
+full.model <- lm(`Change in PCI` ~ `Centerline Miles` + `Lane-miles` + `Area (sy)` + `Area (sm)`
+                 + `PCI 2008` + Population 
+                 + `LOST Active between 08-20` 
+                 + `Measure expired and replaced?` 
+                 + `Period Active` + `LOST Rate` + `Multiple LOSTS Active at same time?` 
+                 + `Average Streets & Roads Percent` + `Average Local Return Percent` 
+                 + `Streets Metric` + `Local Return Metric` 
+                 + `Did the PCI start below 60?` + `Did the PCI fall below 60?` 
+                 + `Pop per lane miles` 
+                 + `Pop per centerline miles`
+                 + `Most Common Climate Zone` 
+                 + `Climate Zone Percentage` 
+                 + CZP1 + CZP2 + CZP3 + CZP4 
+                 + CZP5 + CZP6 + CZP7 + CZP8 
+                 + CZP9 + CZP10 + CZP11 + CZP12 
+                 + CZP13 + CZP14 + CZP15 + CZP16 
+                 + `Democrat %` + `Republican %`
+                 , data = county_data)
 step.model <- stepAIC(full.model, direction = "both", 
-                      trace = FALSE)
+                      trace = TRUE)
 summary(step.model)
 tidy.step.model <- tidy(step.model)
 write.csv(tidy.step.model, "Stepmodel Summary.csv")
+
+
 
 
 log.model <- glm(`Improve or Decline?` ~ 
@@ -110,18 +134,29 @@ summary(lm(PercChange ~ `Streets Metric`
    , data = county_data))
 #doesn't improve it imo
 
+
 summary(lm(`Change in PCI` ~ `PCI 2008`
-           #+ `Democrat %` 
-           + `Republican %`
+           + `Democrat %` 
+           #+ `Republican %`
            #+ `Streets Metric` 
-           + `Local Return Metric`
+           #+ `LOST Active between 08-20`
+           #+ `Average Streets & Roads Percent`
+           + `Average Local Return Percent`
+           #+ `Local Return Metric`
            #+ Population
            #+ `Centerline Miles`
            + `Area (sy)`
            #+ CZP2
            #+ CZP4
-           + `Pop per lane miles`
+           #+ `Pop per lane miles`
+           + `Pop per centerline miles`
            + `Did the PCI start below 60?`
+           #+ `Did the PCI fall below 60?`
            #+ `Multiple LOSTS Active at same time?`
-           , data = county_data))$adj.r.squared
+           , data = county_data))#$adj.r.squared
 
+test.model <- lm(`Change in PCI` ~ `PCI 2008` + `Democrat %` + `Average Local Return Percent` + `Area (sy)` + `Pop per centerline miles` + `Did the PCI start below 60?`
+   , data = county_data)
+county_data$Prediction <- predict.lm(test.model,county_data)
+
+plot(county_data$`Change in PCI`, county_data$Prediction)
